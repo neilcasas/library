204f537929499f6707a1fddf2a35cf497dabf848
Neil Casas <neilalfonzcasas@gmail.com>
2024-02-18 23:04:06 +0800

    added constructor and query selectors

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function Book(){
-    
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
+
+// User Interface
+const addBtn = document.querySelector('#add-button');
+const libraryGrid = document.querySelector('.library-grid');
