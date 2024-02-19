7d6430341a60d37b40b2d6cdf7e2abfd7bebbed4
Neil Casas <neilalfonzcasas@gmail.com>
2024-02-19 21:51:43 +0800

    added function addBookToLibrary()

--- script.js
+++ script.js
@@ -28,3 +28,12 @@ addBtn.addEventListener("click", () => {
 closeModalBtn.addEventListener("click", () => {
     modal.classList.remove("open");
 })
+
+
+function addBookToLibrary() {
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
+    let read = document.getElementById('read').value;
+    let newBook = new Book(title, author, pages, read);
+}
