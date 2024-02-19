1fad317e3bf6506ee63c57d37004655ef236bf71
Neil Casas <neilalfonzcasas@gmail.com>
2024-02-19 21:37:46 +0800

    added open modal onclick function

--- script.js
+++ script.js
@@ -8,6 +8,17 @@ function Book(title, author, pages, read) {
 }
 
 // User Interface
-const addBtn = document.querySelector('#add-button');
+const addBtn = document.querySelector('.add-button');
 const libraryGrid = document.querySelector('.library-grid');
+const openModalBtn = document.querySelector('#open-modal');
+const modal = document.querySelector('.modal');
 
+
+openModalBtn.addEventListener("click", () => {
+    modal.classList.add("open");
+    console.log('clicked');
+})
+
+addBtn.addEventListener("click", () => {
+    modal.classList.remove("open");
+})
