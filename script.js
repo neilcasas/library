929ebf0228d369769d8d280a9a92351485593b9e
Neil Casas <neilalfonzcasas@gmail.com>
2024-02-19 22:05:49 +0800

    fixed bug on click of add book

--- script.js
+++ script.js
@@ -18,15 +18,17 @@ const libraryGrid = document.querySelector('.library-grid');
 // Opening and closing Modal
 openModalBtn.addEventListener("click", () => {
     modal.classList.add("open");
-    console.log('clicked');
 })
 
 addBtn.addEventListener("click", () => {
     modal.classList.remove("open");
+    addBookToLibrary();
+    event.preventDefault();
 })
 
 closeModalBtn.addEventListener("click", () => {
     modal.classList.remove("open");
+    event.preventDefault();
 })
 
 
@@ -36,4 +38,6 @@ function addBookToLibrary() {
     let pages = document.getElementById('pages').value;
     let read = document.getElementById('read').value;
     let newBook = new Book(title, author, pages, read);
+    console.log(newBook);
+    myLibrary.push(newBook);
 }
